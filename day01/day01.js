1f64dc68733d2e12acce2dd1315f4e3664d0ed93
Tarah Z. Tamayo <tarah.tamayo@pillpack.com>
2021-12-01 12:29:20 +0000

    day 1 complete

---
+++ day01/day01.js
@@ -0,0 +1,37 @@
+fs = require('fs')
+let depths = fs.readFileSync('input', 'utf8').split('\n')
+
+console.log("\n\n***** PART 1 *****")
+let total_increase = 0
+let prev = Infinity
+for (depth of depths) {
+    if (parseInt(depth) > prev) total_increase++
+    prev = parseInt(depth)
+}
+console.log(total_increase)
+
+function sum(arr) {
+    out = 0
+    for (num of arr) {
+        out += num
+    }
+    return out
+}
+
+console.log("\n\n***** PART 2 *****")
+total_increase = 0
+prev = Infinity
+let window = []
+for (depth of depths) {
+    let d = parseInt(depth)
+    window.push(d)
+    if (window.length > 3) window.shift()
+    if (window.length === 3) {
+        current = sum(window)
+        if (current > prev) {
+            total_increase++
+        }
+        prev = current
+    }
+}
+console.log(total_increase)
